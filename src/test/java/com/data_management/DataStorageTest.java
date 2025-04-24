68ddc923cd6b154b17b6da934ef3885a8891ccbe
Kallam Schroder <ultraphoenix978@gmail.com>
2025-04-24 16:21:54 +0200

    Fix DataStorageTest to expect 17 records, adjust diastolic BP values to trigger trend alert

--- src/test/java/com/data_management/DataStorageTest.java
+++ src/test/java/com/data_management/DataStorageTest.java
@@ -11,7 +11,6 @@ public class DataStorageTest {
 
     @Test
     void testAddAndGetRecords() throws IOException {
-        // Use a direct path to src/test/resources/test_data.txt
         String filePath = "src/test/resources/test_data.txt";
         DataStorage storage = new DataStorage(new FileDataReader(filePath));
         storage.readData();
@@ -21,6 +20,6 @@ public class DataStorageTest {
 
         Patient patient = patients.get(0);
         List<PatientRecord> records = patient.getRecords(0, Long.MAX_VALUE);
-        assertEquals(13, records.size(), "Expected 13 records from test_data.txt");
+        assertEquals(17, records.size(), "Expected 17 records from test_data.txt");
     }
 }
